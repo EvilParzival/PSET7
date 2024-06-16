1ece89747ecd4e6882f07ad74e05d8bf8d5fd290
./Parzival <parzival.homebrew@gmail.com>
2024-06-16 16:00:19 +0200

    PSET7

---
+++ pset7.bat
@@ -0,0 +1,12 @@
+@echo off
+chcp 65001 > nul
+py -V > nul
+if %errorlevel% NEQ 0 (
+    echo Python 3 is not installed.
+    echo Please install Python 3 and try again.
+    echo https://www.python.org/downloads/
+    echo.
+    pause
+    exit
+)
+py -3 pset7.py
